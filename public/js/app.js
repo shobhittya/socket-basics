d22d2ac5ff76b97cb42dc4ae30ad8e785e4acfa0
Shobhit Tyagi <Shobhit.tya@gmail.com>
2018-04-10 13:34:01 +0530

    Add front end

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,24 @@ socket.on('connection', function() {
     console.log('Connected to Socket.io !');
 });
 
-socket.on('message', function(message) {
+    socket.on('message', function(message) {
     console.log('New Message : ');
     console.log(message.text);
 
-})
+   // jQuery('.messages').append('<p>'  + message.text + '</p>');
+
+});
+
+// //Handles submtting of new messages
+var $form = jQuery('#message-form');
+
+$form.on('submit', function(event) {
+     event.preventDefault();
+
+     var $message =  $form.find('input[name=message]');
+     socket.emit('message', {
+         text : $message.val() 
+     });
+
+    $message.val('');
+});
